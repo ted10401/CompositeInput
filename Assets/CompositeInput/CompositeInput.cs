3cd39f641d734df608bd066d0d0ea37b1d3488d9
GMPC-2171N\2171 <ted10401@livemail.tw>
2021-03-17 14:39:19 +0800

    Upload project

---
+++ Assets/CompositeInput/CompositeInput.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class CompositeInput
+{
+    public float validDuration = 0.1f;
+    public KeyCode[] keyCodes;
+
+    private Action m_onKeyDown;
+    private Action m_onKey;
+    private Action m_onKeyUp;
+
+    private float m_keyDownDuration;
+    private int m_keyDownCount;
+    private bool m_key;
+    private float m_keyUpDuration;
+
+    public void OnKeyDown(Action onKeyDown)
+    {
+        m_onKeyDown = onKeyDown;
+    }
+
+    public void OnKey(Action onKey)
+    {
+        m_onKey = onKey;
+    }
+
+    public void OnKeyUp(Action onKeyUp)
+    {
+        m_onKeyUp = onKeyUp;
+    }
+
+    public void Update(float deltaTime)
+    {
+        m_keyDownDuration -= deltaTime;
+        m_keyUpDuration -= deltaTime;
+
+        m_key = true;
+
+        for (int i = 0; i < keyCodes.Length; i++)
+        {
+            if (Input.GetKeyDown(keyCodes[i]))
+            {
+                if (m_keyDownCount == 0)
+                {
+                    m_keyDownDuration = validDuration;
+                }
+
+                m_keyDownCount++;
+
+                if (m_keyDownCount == keyCodes.Length && m_keyDownDuration > 0)
+                {
+                    m_onKeyDown?.Invoke();
+                }
+            }
+
+            if (!Input.GetKey(keyCodes[i]))
+            {
+                m_key = false;
+            }
+
+            if (Input.GetKeyUp(keyCodes[i]))
+            {
+                if (m_keyDownCount == keyCodes.Length)
+                {
+                    m_keyUpDuration = validDuration;
+                }
+
+                m_keyDownCount--;
+
+                if (m_keyDownCount == 0 && m_keyUpDuration > 0)
+                {
+                    m_onKeyUp?.Invoke();
+                }
+            }
+        }
+
+        if(m_key)
+        {
+            m_onKey?.Invoke();
+        }
+    }
+}
---
+++ Assets/CompositeInput/Demo/Demo_CompositeInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Demo_CompositeInput : MonoBehaviour
+{
+    [SerializeField] private CompositeInput m_compositeInput;
+
+    private void Awake()
+    {
+        m_compositeInput.OnKeyDown(OnKeyDown);
+        m_compositeInput.OnKey(OnKey);
+        m_compositeInput.OnKeyUp(OnKeyUp);
+    }
+
+    private void OnKeyDown()
+    {
+        Debug.Log("OnKeyDown");
+    }
+
+    private void OnKey()
+    {
+        Debug.Log("OnKey");
+    }
+
+    private void OnKeyUp()
+    {
+        Debug.Log("OnKeyUp");
+    }
+
+    private void Update()
+    {
+        m_compositeInput.Update(Time.deltaTime);
+    }
+}
